198955a0b33e398763d9a04486cb3a477fa909aa
davidnjoroge <njorogedavid13@gmail.com>
2019-06-24 22:00:32 +0300

    add router

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("hello world")
+	// router := NewRouter()
+	// http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
