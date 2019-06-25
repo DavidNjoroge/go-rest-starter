09016145f1f12a6710ab81a02b1922628497efcd
davidnjoroge <njorogedavid13@gmail.com>
2019-06-25 22:26:17 +0300

    connect to db and get all api

--- main.go
+++ main.go
@@ -2,53 +2,63 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+var db *gorm.DB
+var err error
+
 type ShopItem struct {
-	Id    string `json:"id,omitempty"`
+	gorm.Model
+	Id    string `json:"id,omitempty";gorm:"primary_key"`
 	Name  string `json:"name,omitempty"`
 	Price string `json:"price,omitempty"`
 }
 
 type ShopItems []ShopItem
 
-var shopItems ShopItems
-
-func main() {
-	shopItems = append(shopItems, ShopItem{Id: "1", Name: "unga", Price: "100"})
-	router := mux.NewRouter()
-	router.HandleFunc("/items", getItems).Methods("GET")
-	router.HandleFunc("/items/{id}", getItem).Methods("GET")
-	router.HandleFunc("/items", createItem).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
 func getItems(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	var shopItems ShopItems
+
+	db.Find(&shopItems)
 	json.NewEncoder(w).Encode(shopItems)
 }
 
 func getItem(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-
-	for _, item := range shopItems {
-		if item.Id == params["id"] {
-
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-
-	json.NewEncoder(w).Encode(ShopItem{})
+	fmt.Fprintf(w, "get item")
 }
 
 func createItem(w http.ResponseWriter, req *http.Request) {
-	var shopItem ShopItem
-	json.NewDecoder(req.Body).Decode(&shopItem)
-	shopItems = append(shopItems, shopItem)
-	json.NewEncoder(w).Encode(shopItem)
+	fmt.Fprintf(w, "get item")
+}
 
+func handleRequests() {
+	router := mux.NewRouter()
+	router.HandleFunc("/items", getItems).Methods("GET")
+	router.HandleFunc("/items/{id}", getItem).Methods("GET")
+	router.HandleFunc("/items", createItem).Methods("POST")
+	fmt.Println("Go server :8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func main() {
+	db, err = gorm.Open("postgres", "host=127.0.0.1 port=5432 user=goshop dbname=goshop password=goshop sslmode=disable")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect to database")
+	}
+	defer db.Close()
+	db.AutoMigrate(&ShopItem{})
+
+	handleRequests()
 }
